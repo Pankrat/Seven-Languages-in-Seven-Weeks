430cdf510bd4b0ec908e8c13397d2b22b23453b4
Pankrat <lhaehne@gmail.com>
2011-12-11 00:16:35 +0100

    make function tail recursive

--- erlang/words.erl
+++ erlang/words.erl
@@ -10,21 +10,22 @@ whitespace($\n) -> 1;
 whitespace($\t) -> 1;
 whitespace(_) -> 0.
 
-count_ws([]) -> 0;
-count_ws([_ | []]) -> 0;
-count_ws([X | [Y | T]]) ->
+count_ws([], Acc) -> Acc;
+count_ws([_ | []], Acc) -> Acc;
+count_ws([X | [Y | T]], Acc) ->
     Wx = whitespace(X),
     Wy = whitespace(Y),
     if
         Wy - Wx == 1 ->
-            1 + count_ws([Y | T]);
+            EoW = 1;
         true ->
-            count_ws([Y | T])
-    end.
+            EoW = 0
+    end,
+    count_ws([Y | T], Acc+EoW).
 
 not_empty("") -> 0;
 not_empty(_) -> 1.
 
 count(Text) -> 
     Stripped = string:strip(Text),
-    not_empty(Stripped) + count_ws(Stripped).
+    not_empty(Stripped) + count_ws(Stripped, 0).
